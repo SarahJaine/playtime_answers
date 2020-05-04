5d0e9f63cfced8413b781296386832ef703d0ef8
Sarah Jaine Szekeresh <sj@glitter.local>
2020-05-03 22:57:08 -0500

    correct spacing

--- lesson4_contacts_list.py
+++ lesson4_contacts_list.py
@@ -4,20 +4,20 @@
 
 # Background: You have a dictionary with people's contact information.  You want to display that information as an HTML table.
 contacts = {
-	'Shannon': {
-		'phone': '202-555-1234', 
-		'twitter': '@svt827', 
-		'github': '@shannonturner'
-	},
-	'Beyonce': {
-		'phone': '303-404-9876', 
-		'twitter': '@beyonce', 
-		'github': '@bey'
-	},
+    'Shannon': {
+        'phone': '202-555-1234',
+        'twitter': '@svt827',
+        'github': '@shannonturner'
+    },
+    'Beyonce': {
+        'phone': '303-404-9876',
+        'twitter': '@beyonce',
+        'github': '@bey'
+    },
     'Tegan and Sara': {
-    	'phone': '301-777-3313', 
-    	'twitter': '@teganandsara', 
-    	'github': '@heartthrob'
+        'phone': '301-777-3313',
+        'twitter': '@teganandsara',
+        'github': '@heartthrob'
     }
 }
 
@@ -31,10 +31,10 @@ contacts = {
 #   Github: @bey
 
 for name, info in contacts.items():
-	print('''{0}'s contact information is:
-		Phone: {1}
-		Twitter: {2}
-		Github: {3}'''.format(name, info['phone'], info['twitter'], info['github']))
+    print('''{0}'s contact information is:
+        Phone: {1}
+        Twitter: {2}
+        Github: {3}'''.format(name, info['phone'], info['twitter'], info['github']))
 
 
 # Goal 2:  Display that information as an HTML table.
@@ -54,14 +54,14 @@ for name, info in contacts.items():
 
 print('<table border="1">')
 for name, info in contacts.items():
-	print(str("""\t<tr>
-		<td colspan="2"> {0} </td>
-	</tr>
-	<tr>
-		<td> Phone: {1} </td>
-		<td> Twitter: {2} </td>
-		<td> Github: {3} </td>
-	</tr>""".format(name, info['phone'], info['twitter'], info['github'])))
+    print(str("""\t<tr>
+        <td colspan="2"> {0} </td>
+    </tr>
+    <tr>
+        <td> Phone: {1} </td>
+        <td> Twitter: {2} </td>
+        <td> Github: {3} </td>
+    </tr>""".format(name, info['phone'], info['twitter'], info['github'])))
 print('</table>')
 
 
@@ -69,39 +69,39 @@ print('</table>')
 
 file_contents = '<table border="1">'
 for name, info in contacts.items():
-	file_contents += """\t
-	<tr>
-		<td colspan="2"> {0} </td>
-	</tr>
-	<tr>
-		<td> Phone: {1} </td>
-		<td> Twitter: {2} </td>
-		<td> Github: {3} </td>
-	</tr>""".format(name, info['phone'], info['twitter'], info['github'])
+    file_contents += """\t
+    <tr>
+        <td colspan="2"> {0} </td>
+    </tr>
+    <tr>
+        <td> Phone: {1} </td>
+        <td> Twitter: {2} </td>
+        <td> Github: {3} </td>
+    </tr>""".format(name, info['phone'], info['twitter'], info['github'])
 file_contents += '\n</table>'
 
 with open('contacts.html', 'w') as contacts_file:
-	contacts_file.write(file_contents)
+    contacts_file.write(file_contents)
 
 
 # Goal 4: Instead of reading in the contacts from the dictionary above, 
 # read them in from contacts.csv, which you can find in lesson_07_(files).
 
 with open('contacts.csv') as contacts_file:
-	contacts = contacts_file.read()
+    contacts = contacts_file.read()
 
 contact_directory = {}
 i = 0
 
 # split file contents by row
 for row in contacts.split('\n'):
-	# split row by column
-	if row != 0:
-		name, phone, github, twitter = row.split(',')
-		contact_directory[name] = {
-			'Phone': phone,
-			'Github': github,
-			'Twitter': twitter
-		}
-	i += 1
+    # split row by column
+    if row != 0:
+        name, phone, github, twitter = row.split(',')
+        contact_directory[name] = {
+            'Phone': phone,
+            'Github': github,
+            'Twitter': twitter
+        }
+    i += 1
 print(contact_directory)
